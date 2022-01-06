1a49941d5c9a31cca136a1fcff2e87278b2bb02e
Vladimir Vorozhtcov <lastlilith.2014@gmail.com>
2022-01-06 19:05:18 +0900

    Created empty fragments for each view

---
+++ app/src/main/java/com/example/compositiongame/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,22 @@
+package com.example.compositiongame.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.compositiongame.R
+
+class ChooseLevelFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/compositiongame/presentation/GameFinishedFragment.kt
@@ -0,0 +1,23 @@
+package com.example.compositiongame.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.compositiongame.R
+
+class GameFinishedFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    }
+
+}

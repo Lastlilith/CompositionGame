fd3548c30d431f80770bc11c5332c23d2f27512c
Vladimir Vorozhtcov <lastlilith.2014@gmail.com>
2022-02-09 20:42:16 +0900

    Added background sound for MainActivity.kt

--- app/src/main/java/com/example/compositiongame/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/compositiongame/presentation/activities/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.compositiongame.presentation.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.compositiongame.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var backgroundMusic: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        backgroundMusic = MediaPlayer.create(this, R.raw.background)
+        backgroundMusic.start()
+
+        //Workaround for not working isLooping
+        backgroundMusic.setOnCompletionListener {
+            backgroundMusic.start()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        backgroundMusic.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        backgroundMusic.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        backgroundMusic.release()
     }
 }
